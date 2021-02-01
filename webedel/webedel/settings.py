dec0370cb6ed1d28605ef4fb873d1371eaaa35a1
Tobias Gaston Silva <tobias_gaston_silva@hotmail.com.ar>
2021-01-31 21:36:59 -0300

    version4

--- webedel/webedel/settings.py
+++ webedel/webedel/settings.py
@@ -119,5 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR/"static",'/secciones/assets',
-]
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
